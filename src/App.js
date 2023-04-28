2599005a2726d4f96d0d7835cdf088f982532935
unknown <yurimarcelo2018@gmail.com>
2023-04-27 23:02:41 -0300

    Adicionando componente do formulario

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 
-import Banner from './componentes/Banner/Banner.js';
+import Banner from './componentes/Banner';
+import CampoTexto from './componentes/CampoTexto';
 
 function App() {
   return (
     <div className="App">
       <Banner></Banner>
+      <CampoTexto label ="Nome"/>
+      <CampoTexto label ="Cargo"/>
+      <CampoTexto label ="Imagem"/>
     </div>
   );
+
 }
 
 export default App;
+  
---
+++ src/componentes/CampoTexto/index.js
@@ -0,0 +1,12 @@
+import './CampoTexto.css'
+const CampoTexto = () => {
+    return (
+        <section className="campo__texto">
+            <label></label>
+            <input placeholder='Digite o seu nome'></input>
+        </section>
+
+    )
+}
+
+export default CampoTexto
